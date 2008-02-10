5470c1c608b15103bbfd709b5c08179290249280
pstieber <pstieber@d001d500-7bcc-4352-ac0f-06d6f3fd1383>
2008-02-10 04:11:09 +0000

    Added code for obtaining the index of a command line option.
    git-svn-id: https://svn.code.sf.net/p/jazzplusplus/code/trunk/jazz@214 d001d500-7bcc-4352-ac0f-06d6f3fd1383

---
+++ src/GetOptionIndex.cpp
@@ -0,0 +1,17 @@
+#include "wxWidgets.h"
+
+#include "JazzPlusPlusApplication.h"
+
+int GetOptionIndex(const wxString& Option)
+{
+  int ArgumentIndex = 0;
+
+  for (int Index = 1; Index < ::wxGetApp().argc; ++Index)
+  {
+    if (::wxGetApp().argv[Index] == Option)
+    {
+      ArgumentIndex = Index;
+    }
+  }
+  return ArgumentIndex;
+}
---
+++ src/GetOptionIndex.h
@@ -0,0 +1,6 @@
+#ifndef JZ_GETOPTIONINDEX_H
+#define JZ_GETOPTIONINDEX_H
+
+int GetOptionIndex(const wxString& Option);
+
+#endif // !defined(JZ_GETOPTIONINDEX_H)

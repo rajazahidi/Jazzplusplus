00bf853fe122f4db611b3b438d1aa91d70d6b95f
pstieber <pstieber@d001d500-7bcc-4352-ac0f-06d6f3fd1383>
2008-03-30 15:37:16 +0000

    Added some code for reading and writing MIDI files in ASCII format for debug purposes.  This was ported from version 4.1.3 in branches.
    git-svn-id: https://svn.code.sf.net/p/jazzplusplus/code/trunk/jazz@374 d001d500-7bcc-4352-ac0f-06d6f3fd1383

---
+++ src/AsciiMidiFile.cpp
@@ -0,0 +1,192 @@
+//*****************************************************************************
+// The JAZZ++ Midi Sequencer
+//
+// Copyright (C) 1994-2000 Andreas Voss and Per Sigmond, all rights reserved.
+// Modifications Copyright (C) 2008 Peter J. Stieber
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//*****************************************************************************
+
+#include "WxWidgets.h"
+
+#include "AsciiMidiFile.h"
+
+//*****************************************************************************
+//*****************************************************************************
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+int JZAsciiRead::Open(const char* pFileName)
+{
+  int TrackCount, TicksPerQuarter;
+  if (
+    fscanf(
+      mpFd,
+      "Tracks %d, TicksPerQuarter %d\n",
+      &TrackCount,
+      &TicksPerQuarter) != 2)
+  {
+    return 0;
+  }
+  return TrackCount;
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+JZEvent* JZAsciiRead::Read()
+{
+  JZEvent* pEvent = 0;
+
+  long Clock;
+  int sta, cha, Length;
+  if (fscanf(mpFd, "%6lu %02x %2d %d ", &Clock, &sta, &cha, &Length) != 4)
+  {
+    return pEvent;
+  }
+
+  unsigned char* pBuffer = new unsigned char[Length];
+  for (int i = 0; i < Length; ++i)
+  {
+    int d;
+    fscanf(mpFd, "%02x ", &d);
+    pBuffer[i] = (unsigned char)d;
+  }
+
+  switch (sta)
+  {
+    case StatUnknown:
+      break;
+
+    case StatKeyOff:
+      pEvent = new tKeyOff(Clock, cha, pBuffer[0]);
+      break;
+
+    case StatKeyOn:
+      pEvent = new tKeyOn(Clock, cha, pBuffer[0], pBuffer[1]);
+      break;
+
+    case StatControl:
+      pEvent = new tControl(Clock, cha, pBuffer[0], pBuffer[1]);
+      break;
+
+    case StatPitch:
+      pEvent = new tPitch(Clock, cha, pBuffer[0], pBuffer[1]);
+      break;
+
+    case StatProgram:
+      pEvent = new tProgram(Clock, cha, pBuffer[0]);
+      break;
+
+    case StatText:
+      pEvent = new tText(Clock, pBuffer, Length);
+      break;
+
+    case StatTrackName:
+      pEvent = new tTrackName(Clock, pBuffer, Length);
+      break;
+
+    case StatMarker:
+      pEvent = new tMarker(Clock, pBuffer, Length);
+      break;
+
+    case StatEndOfTrack:
+      break;
+
+    case StatSetTempo:
+      pEvent = new tSetTempo(Clock, pBuffer[0], pBuffer[1], pBuffer[2]);
+      break;
+
+    case StatTimeSignat:
+      pEvent = new tTimeSignat(
+        Clock,
+        pBuffer[0],
+        pBuffer[1],
+        pBuffer[2],
+        pBuffer[3]);
+      break;
+
+    case StatSysEx:
+      pEvent = new tSysEx(Clock, pBuffer, Length);
+      break;
+  }
+
+  delete [] pBuffer;
+
+  return pEvent;
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+int JZAsciiRead::NextTrack()
+{
+  return fscanf(mpFd, "NextTrack\n") == 0;
+}
+
+//*****************************************************************************
+// Description:
+//   Ascii-Output (debug)
+//*****************************************************************************
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+int JZAsciiWrite::Open(
+  const char* pFileName,
+  int TrackCount,
+  int TicksPerQuarter)
+{
+  if (!JZWriteBase::Open(pFileName, TrackCount, TicksPerQuarter))
+  {
+    return 0;
+  }
+
+  fprintf(
+    mpFd,
+    "Tracks %d, TicksPerQuarter %d\n",
+    TrackCount,
+    TicksPerQuarter);
+
+  return TrackCount;
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+int JZAsciiWrite::Write(JZEvent* pEvent, unsigned char* pData, int Length)
+{
+  tChannelEvent *ce;
+
+  fprintf(mpFd, "%6ld %02x ",  pEvent->GetClock(), pEvent->Stat);
+  if ((ce = pEvent->IsChannelEvent()) != 0)
+  {
+    fprintf(mpFd, "%2d ",  ce->Channel);
+  }
+  else
+  {
+    fprintf(mpFd, "-1 ");
+  }
+
+  fprintf(mpFd, "%d ", Length);
+  for (int i = 0; i < Length; ++i)
+  {
+    fprintf(mpFd, "%02x ", pData[i]);
+  }
+  fprintf(mpFd, "\n");
+
+  return 0;
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+void JZAsciiWrite::NextTrack()
+{
+  fprintf(mpFd, "NextTrack\n");
+}

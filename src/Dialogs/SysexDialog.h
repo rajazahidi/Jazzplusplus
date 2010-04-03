65eeda73198c47daf40f3533d01b84bbe95b1819
pstieber <pstieber@d001d500-7bcc-4352-ac0f-06d6f3fd1383>
2010-04-03 02:35:53 +0000

    Added a new sysex dialog.
    git-svn-id: https://svn.code.sf.net/p/jazzplusplus/code/trunk/jazz@741 d001d500-7bcc-4352-ac0f-06d6f3fd1383

---
+++ src/Dialogs/SysexDialog.h
@@ -0,0 +1,71 @@
+//*****************************************************************************
+// The JAZZ++ Midi Sequencer
+//
+// Copyright (C) 2009 Peter J. Stieber, all rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//*****************************************************************************
+
+#ifndef JZ_SYSEXDIALOG_H
+#define JZ_SYSEXDIALOG_H
+
+#include <wx/dialog.h>
+
+class JZKnob;
+class JZKnobEvent;
+class tKeyOn;
+class wxStaticText;
+class wxTextCtrl;
+
+//*****************************************************************************
+//*****************************************************************************
+class JZSysexDialog : public wxDialog
+{
+  public:
+
+    JZSysexDialog(tKeyOn* pEvent, wxWindow* pParent);
+
+  private:
+
+    virtual bool TransferDataToWindow();
+
+    virtual bool TransferDataFromWindow();
+
+    void OnVelocityChange(JZKnobEvent& Event);
+
+    void OnOffVelocityChange(JZKnobEvent& Event);
+
+    void OnChannelChange(JZKnobEvent& Event);
+
+    void OnHelp(wxCommandEvent& Event);
+
+  private:
+
+    tKeyOn* mpEvent;
+
+    wxTextCtrl* mpPitchEdit;
+    wxStaticText* mpVelocityValue;
+    JZKnob* mpVelocityKnob;
+    wxStaticText* mpOffVelocityValue;
+    JZKnob* mpOffVelocityKnob;
+    wxTextCtrl* mpLengthEdit;
+    wxStaticText* mpChannelValue;
+    JZKnob* mpChannelKnob;
+    wxTextCtrl* mpClockEdit;
+
+  DECLARE_EVENT_TABLE();
+};
+
+#endif // !defined(JZ_SYSEXDIALOG_H)
